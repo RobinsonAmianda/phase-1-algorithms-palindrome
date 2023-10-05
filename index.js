16a0ec3048548f294e708c1f10b233fb86c3ad7d
Robinson Amianda <robinsonamianda5@gmail.com>
2023-10-04 17:30:58 -0700

    completed assignment

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ isPalindrome("ab")
 /*
   Add written explanation of your solution here
 */
-
+//Here,we first create a function and pass the parameter to be word
+//For us to know if a word is palindrome,this is what we do:
+//   1.We split the word
+//   2..We reverse the word
+//   3.We join the word.
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
